060c654421ad23286bf36be8eba1ec1b195d8626
Laurie Linz <lvlinz@comcast.net>
2016-03-22 20:18:10 -0600

    bah

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'bottles#number(99)'
+  root 'bottles#number'
   get 'bottles/:number_of_bottles' => 'bottles#number', as: 'bottles'
   get 'take/:number_of_bottles' => 'bottles#take', as: 'take'
   get 'store' => 'bottles#store'
